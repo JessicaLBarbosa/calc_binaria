f57d3ffc70f0c246ff95b313f55890ba192900c4
Jessica Barbosa <lbarbosa.jessica@gmail.com>
2020-09-24 22:38:58 -0300

    criacao componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DesenvolvedoresComponent } from './desenvolvedores/desenvolvedores.component';
+import { TecladoComponent } from './teclado/teclado.component';
+import { HistoricoComponent } from './historico/historico.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DesenvolvedoresComponent,
+    TecladoComponent,
+    HistoricoComponent
   ],
   imports: [
     BrowserModule,
