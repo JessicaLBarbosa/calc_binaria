7246a5976922e1c2442f7d1eb5b22f4a9e48525f
Jesse <44973365+JessicaLBarbosa@users.noreply.github.com>
2020-10-03 00:40:25 -0300

    logica calculadora
    logica da calculadora - soma de binarios

--- src/app/teclado/teclado.component.ts
+++ src/app/teclado/teclado.component.ts
@@ -1,15 +1,51 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { ValidacaoService } from "../validacao.service";
+import { DataService } from "../data.service";
 
 @Component({
-  selector: 'app-teclado',
-  templateUrl: './teclado.component.html',
-  styleUrls: ['./teclado.component.css']
+  selector: "app-teclado",
+  templateUrl: "./teclado.component.html",
+  styleUrls: ["./teclado.component.css"],
 })
 export class TecladoComponent implements OnInit {
+  constructor(private validacao: ValidacaoService, private data: DataService) {}
 
-  constructor() { }
+  public resultado: string = "";
+  public operacao: string = "";
+  public historico: string = "";
 
   ngOnInit(): void {
+    this.historico = this.data.completeMessage;
   }
 
+  ngOnDestroy() {
+    this.data.message = this.operacao;
+    this.data.completeMessage = this.historico;
+  }
+
+  //limpa o resultado
+  limpa() {
+    this.resultado = "";
+  }
+  //Verifica se o caractere passado é um operador
+
+  public montarResultado(digito: string): void {
+    if (this.validacao.ehValidaExpressao(digito, this.resultado)) {
+      this.resultado += digito;
+    }
+  }
+
+  public finalizar(): void {
+    let vetorBin = this.resultado.split("+");
+    let soma: number = 0;
+    vetorBin.forEach((element) => {
+      soma += parseInt(element, 2);
+      this.operacao += element + "+";
+    });
+    this.resultado = parseInt(soma.toString(), 10).toString(2);
+    this.operacao =
+      this.operacao.substring(0, this.operacao.length - 1) +
+      "=" +
+      this.resultado;
+  }
 }
